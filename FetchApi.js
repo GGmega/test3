f1de81d5f4f0cc58e11bf857a929bb07d00dd35f
GGmega <qqmega@gmail.com>
2015-07-25 00:56:20 +0800

    fix bugs

--- FetchApi.js
+++ FetchApi.js
@@ -1,28 +1,36 @@
+
 "use strict";
 class FetchApi {
 
     constructor(url) {
         this.successList = [];
 
-
         var isReady = false;
         var pmt;
 
         this.ready =  function() {
+            function _f() {
+                pmt = fetch(this._request.clone());
+                pmt.then(this.finishFetch);
+            }
+
             if (!isReady) {
                 isReady = true;
-                pmt = fetch(this._request);
-                pmt.then(this.finishFetch);
+                Promise.resolve().then(_f.bind(this));
             }
         };
 
         this.finishFetch = function() {
             var i;
+            pmt = pmt.then(function(res) {
+                if (!res.ok) throw new Error(res.status + " _-_ " + res.statusText + " : " + res.url);
+            });
             for (i = 0; i < this.successList.length; ++i) {
                 pmt = pmt.then(this.successList[i]);
             }
             if (i === 0) pmt = pmt.then(function(res) {console.log(res)});
             pmt.catch(this._error || function(rej) {console.log(rej)});
+
             isReady = false;
         }.bind(this);
 
@@ -37,22 +45,32 @@ class FetchApi {
     get(url) {
         this.url = url;
         this.method = "GET";
+
+        return this;
     }
 
     post(url, str) {
         this.url = url;
-        this.body = str;
+        if (typeof str === "string") this.body = str;
         this.method = "POST";
+
+        return this;
     }
 
     pre() {}
 
-    success(fn) {}
+    success(fn) {
+        if (typeof fn === "function") this.successList.push(fn);
+
+        return this;
+    }
 
     error(err) {
         if (typeof err === "function") {
             this._error = err;
         }
+
+        return this;
     }
 
     get request() {
@@ -75,7 +93,7 @@ class FetchApi {
     set headers(hds) {
         if (hds instanceof Headers && hds !== this._headers) {
             this._headers = hds;
-            this.request = new Request(this._request.url, {
+            this._request = new Request(this._request.url, {
                 method : this._request.method,
                 headers : this._headers,
                 body : this._body
@@ -91,7 +109,8 @@ class FetchApi {
     set body(bd) {
         if (typeof bd === "string" && bd !== this._body) {
             this._body = bd;
-            this.request = new Request(this._request.url, {
+            this._headers.set("Content-Type", "application/x-www-form-urlencoded");
+            this._request = new Request(this._request.url, {
                 method : (/GET|HEAD/i.test(this._request.method)) ? "POST" : this._request.method,
                 headers : this._headers,
                 body : this._body
@@ -106,11 +125,20 @@ class FetchApi {
 
     set url(url) {
         if (typeof url === "string" && url !== this._request.url) {
-            this.request = new Request(url, {
-                method : this._request.method,
-                headers : this._headers,
-                body : this._body
-            });
+            if (/GET|HEAD/i.test(this._request.method)) {
+                this._request = new Request(url, {
+                    method : this._request.method,
+                    headers : this._headers
+                });
+            }
+            else {
+                this._request = new Request(url, {
+                    method: this._request.method,
+                    headers: this._headers,
+                    body: this._body
+                });
+            }
+
             this.ready();
         }
     }
